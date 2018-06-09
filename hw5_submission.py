2a3ff1a98fc53f229344abd9b04e3d4e3f54edfc
zsb87 <zhangshibo87@gmail.com>
2018-06-09 01:18:24 -0500

    no_transform

--- hw5_submission.py
+++ hw5_submission.py
@@ -14,6 +14,7 @@ file and a .pdf file explaining your network and results.
 
 # X size: torch.Size([60000, 28, 28])
 # y set: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
 """
 
 import os
@@ -116,12 +117,13 @@ def validate(model, val_loader, criterion):
     for i, (X,y) in enumerate(val_loader):
         if cuda:
             X, y = X.cuda(), y.cuda()
+        # X, y = Variable(X, volatile=True), Variable(y)
         X, y = Variable(X), Variable(y)
         output = model(X)
         loss = criterion(output, y)
 
         running_loss += loss.item()
-        pred = output.data.max(1, keepdim=True)[1]
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         running_correct += pred.eq(y.data.view_as(pred)).cpu().sum()
     
     val_acc = running_correct.numpy()/len(val_loader.dataset)
@@ -142,38 +144,66 @@ if __name__ == '__main__':
     parser.add_argument("--nworkers", type=int, default=4, help="number of workers")
     args = parser.parse_args()
 
-    cuda = not args.nocuda and torch.cuda.is_available()
+    cuda = not args.nocuda and torch.cuda.is_available() # use cuda
 
     seed = 1
 
+    # set seed
     np.random.seed(seed)
     torch.manual_seed(seed)
     if cuda:
         torch.cuda.manual_seed(seed)
 
+    mdict = sio.loadmat('data')
 
-    train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                            # utils.RandomRotation(),
-                                            utils.RandomTranslation(),
-                                            # utils.RandomVerticalFlip(),
-                                            transforms.ToTensor()])
+    X = mdict['X'].astype('float32').reshape((60000, 784))/255
+    y = mdict['y'].reshape((60000, 1))
 
-    val_transforms = transforms.Compose([transforms.ToTensor()])
+    XY = np.hstack((X,y))
+    XY_train, XY_val = tt_split_pseudo_rand(XY, 0.9, seed)
+    X_train = XY_train[:,:-1]
+    Y_train = XY_train[:,-1]
+    X_val = XY_val[:,:-1]
+    y_val = XY_val[:,-1]
+
+    X = torch.from_numpy(X_train)
+    y = torch.from_numpy(Y_train.squeeze()).long()
+
+    X_val = torch.from_numpy(X_val)
+    y_val = torch.from_numpy(y_val.squeeze()).long()
+    train_data = utils_data.TensorDataset(X, y)
+    val_data = utils_data.TensorDataset(X_val, y_val)
 
 
-    train_data = HW5_dataset('data', train = 1, transform = train_transforms)
-    val_data = HW5_dataset('data', train = 0, transform = val_transforms)
+
+    # train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+    #                                         # utils.RandomRotation(),
+    #                                         utils.RandomTranslation(),
+    #                                         # utils.RandomVerticalFlip(),
+    #                                         transforms.ToTensor()
+    #                                         # transforms.Normalize((0.1307,), (0.3081,))
+    #                                         ]
+    #                                         )
+    # val_transforms = transforms.Compose([transforms.ToTensor()
+    #                         ])
+    # train_data = HW5_dataset('data', train = 1, transform = train_transforms)
+    # val_data = HW5_dataset('data', train = 0, transform = val_transforms)
+
+
 
     train_loader = utils_data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True,
                                         num_workers=args.nworkers)
     val_loader = utils_data.DataLoader(val_data, batch_size=args.batch_size, shuffle=False, 
-                                        num_workers=args.nworkers)     
+                                        num_workers=args.nworkers)    
+     
 
 
 
     net = model.__dict__[args.model]()
     print(net)
+
     
+    # Change optimizer for finetuning    
     optimizer = torch.optim.Adam(net.parameters())
     criterion = torch.nn.CrossEntropyLoss()
     if cuda:
@@ -191,6 +221,7 @@ if __name__ == '__main__':
 
         print(stats)
 
+        #early stopping and save best model
         if val_acc > 0.92:
             torch.save({'arch': args.model,'state_dict': net.state_dict()}, 
                             'trained_model_{}_{}_epoch{}_acc{}.pt'.format(args.model, epoch, val_loss))
